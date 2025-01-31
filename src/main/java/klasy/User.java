4fe48055063f02e9d993a330e947074cca669477
6e776c7a <patpaj3@gmail.com>
2025-01-31 23:27:56 +0100

    123

--- src/main/java/klasy/User.java
+++ src/main/java/klasy/User.java
@@ -1,14 +1,12 @@
 package klasy;
 
 public class User extends Dane {
-    private String firma;
     private String name;
     private String surname;
     private String phone;
-    public User(String password, String login , String firma,String name, String surname, String email, String phone, String miasto, String ulica, String nr_bud) {
+    public User(String password, String login,String name, String surname, String email, String phone, String miasto, String ulica, String nr_bud) {
         this.password = password;
         this.login = login;
-        this.firma = firma;
         this.name = name;
         this.surname = surname;
         this.email = email;
