3cddb3ac0285a44bd88d4831e907459f164d7e68
Szlesniak <szymek.les@o2.pl>
2025-01-31 23:38:15 +0100

    kod

---
+++ src/main/java/klasy/Dane.java
@@ -0,0 +1,10 @@
+package klasy;
+
+public abstract class Dane {
+    public String login;
+    public String password;
+    public String miasto;
+    public String ulica;
+    public String nr_bud;
+    public String email;
+}
