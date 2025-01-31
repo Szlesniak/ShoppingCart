8d71551b6f6fe429c458f9c988a356b36c52767c
Szlesniak <szymek.les@o2.pl>
2025-02-01 00:20:29 +0100

    kod

--- src/main/java/klasy/Salesman.java
+++ src/main/java/klasy/Salesman.java
@@ -3,6 +3,12 @@ package klasy;
 public class Salesman extends Dane {
     String name;
     String company;
+    String NIP;
+    String nr_konta;
+    public Salesman(String name, String company, String NIP, String nr_konta, String login, String password, String miasto, String ulica, String nr_bud, String email){
+
+    }
+
 
 
 }
