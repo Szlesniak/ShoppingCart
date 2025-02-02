5c8f9f60a0c19577527e60c4274583917041d030
6e776c7a <patpaj3@gmail.com>
2025-02-02 14:45:26 +0100

    123

--- src/main/java/klasy/Cart.java
+++ src/main/java/klasy/Cart.java
@@ -1,4 +1,23 @@
 package klasy;
 
+import java.util.ArrayList;
+
 public class Cart {
+    private ArrayList<Product> products = new ArrayList<Product>();
+    public void addProduct(Product product) {
+        products.add(product);
+    }
+    public void removeProduct(Product product) {
+        products.remove(product);
+    }
+    public ArrayList<Product> getProducts() {
+        return products;
+    }
+    public double getTotalPrice() {
+        double totalPrice = 0;
+        for (Product product : products) {
+            totalPrice += product.getTotalPrice();
+        }
+        return totalPrice;
+    }
 }
